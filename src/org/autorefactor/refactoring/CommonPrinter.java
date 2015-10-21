e0ad80402b0396808c5b3346ae21fc428dae76d3
Jean-Noël Rouvignac <jn.rouvignac@gmail.com>
2015-10-22 00:18:38 +0200

    Step1: extracted containsAnyIgnoreCase()
    Surprise! this removed code duplication!!

--- src/org/autorefactor/refactoring/CommonPrinter.java
+++ src/org/autorefactor/refactoring/CommonPrinter.java
@@ -7,19 +7,7 @@ public class CommonPrinter {
 
     static boolean printIfCommon(List<String> names1, List<String> names2, boolean checkNames, boolean checkAdditional) {
         if (checkNames) {
-            boolean found = false;
-            for (String s1 : names1) {
-                for (String s2 : names2) {
-                    if (s1.equalsIgnoreCase(s2)) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (found) {
-                    break;
-                }
-            }
-            if (found) {
+            if (containsAnyIgnoreCase(names1, names2)) {
                 System.out.println("Common name exist");
                 return true;
             }
@@ -29,19 +17,7 @@ public class CommonPrinter {
                 for (String name : names2) {
                     copyNames.add(name);
                 }
-                boolean found = false;
-                for (String s1 : names1) {
-                    for (String s2 : copyNames) {
-                        if (s1.equalsIgnoreCase(s2)) {
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (found) {
-                        break;
-                    }
-                }
-                if (found) {
+                if (containsAnyIgnoreCase(names1, copyNames)) {
                     System.out.println("Same name exist");
                 } else {
                     System.out.println("No similar name exist");
@@ -50,6 +26,17 @@ public class CommonPrinter {
             }
         }
         System.out.println("No common name");
+        return false;
+    }
+
+    private static boolean containsAnyIgnoreCase(List<String> names1, List<String> names2) {
+        for (String s1 : names1) {
+            for (String s2 : names2) {
+                if (s1.equalsIgnoreCase(s2)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
